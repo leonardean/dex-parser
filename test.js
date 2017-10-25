ba772360a6f84f38e72f7fedb9ca46713b1b3eb5
Zhanwen xu <leon.xu@kii.com>
2017-10-25 15:01:42 +0800

    added log

--- test.js
+++ test.js
@@ -64,18 +64,21 @@ listenToLogs((path) => {
 						})
 
 						data.events.map((event) => {
+							console.log(1)
 							if (event.eventActivity == '0') {
 								if (prevEvents == undefined || prevEvents == [])
 									console.log("previous events are empty")
 								else {
 									prevEvents.every((prevEvent, index) => {
+										console.log(2)
 										if (prevEvent.eventIdentification == event.eventIdentification && prevEvent.eventActivity == '1') {
+											console.log(3)
 											updateEvent(event.eventIdentification, 'FIXED', ()=>{
 												prevEvents.splice(index, 1)
-												return false
 											})
-										}
-										return true
+											return false
+										} else
+											return true
 									})
 								}
 							} else if (event.eventActivity == '1') {
